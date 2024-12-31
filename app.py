e1a706b38c4b9fc605d043c8628162548b0e6809
aicapstone <aicapstoneproject721@gmail.com>
2024-12-31 10:43:40 +0530

    commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import streamlit as st
-import SpeechRecognition as sr
+import streamlit as st # type: ignore 
+import SpeechRecognition as sr # type: ignore
 
 # Initialize the recognizer
 recognizer = sr.Recognizer()
